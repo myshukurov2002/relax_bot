8d31a124a7d090b5e98738fc77cfbe6c153ca725
Muhammad Yusuf <myshukurov@gmail.com>
2024-11-07 12:30:12 +0500

    first commit

---
+++ src/main/java/com/company/Components.java
@@ -0,0 +1,25 @@
+package com.company;
+
+public interface Components {
+    String OVERVIEW = """
+            🏪ORHIDEYA HOTEL&SPA🏪
+            Assalomu alaykum! 😊\s
+            Hurmatli mijoz, bizni tanlaganingizdan xursandmiz! Sizga avtomatik bot javob bermoqda 👩🏻‍💻\s
+            📍 Bizning manzil va narxlar bilan tanishishingiz mumkin.\s
+            Agar ma'qul kelsa, lokatsiyamiz orqali bemalol tashrif buyurishingiz mumkin.\s
+            Biz 24/7 sizning xizmatdamiz! 😊""";
+
+    String START = "/start";
+    String LOCATION = "📍 Manzil Olish";
+    String PRICES = "\uD83D\uDCB8 Narxlar";
+    String PRICE_1 = "\uD83D\uDCB0 450 mingdan 3 million so'mgacha";
+    String PRICE_2 = "\uD83D\uDCB0 500 mingdan 13 million so'mgacha";
+    String PRICE_3 = "\uD83D\uDCB0 450 mingdan 3 million so'mgacha\n\uD83D\uDCB0 500 mingdan 13 million so'mgacha";
+
+    Double longitude = 69.257473;
+    Double latitude = 41.287429;
+    String ERROR = "Xato Buyruq! Menyuga qaytish uchun /start bosing! \uD83D\uDE42\n/start\n/start";
+
+    String PHONE_1 = "\n\n☎\uFE0F Raqamlar:\n \uD83C\uDFEA Hotel: +998992049393\n \uD83D\uDC86\uD83C\uDFFB Massage 24/7: +998992049393";
+    String PHONE_2 = "+998981015702";
+}
